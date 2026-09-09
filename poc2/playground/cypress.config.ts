922543ea87411fa873d38c36711e86b082f35eb2
ChuDucAnh242002 <chuducanh12345@gmail.com>
2026-09-09 15:35:19 +0300

    feat: add component testing setup with Cypress configuration and example files

--- poc2/playground/cypress.config.ts
+++ poc2/playground/cypress.config.ts
@@ -4,4 +4,11 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4173",
   },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
 });
